b445f8dcd3f1cd2d918315e5dc113b1a1b205a81
Jerome Plumat <j.plumat@gmail.com>
2016-04-05 20:53:51 -0700

    initialisation of the project with CMake and cppunit

---
+++ tests/headers/BasicTest.h
@@ -0,0 +1,33 @@
+//
+// Created by plumat on 4/5/16.
+//
+
+#ifndef SSO_BASICTEST_H
+#define SSO_BASICTEST_H
+
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace CppUnit;
+using namespace std;
+
+class BasicTest : public CPPUNIT_NS::TestFixture{
+	CPPUNIT_TEST_SUITE(BasicTest);
+
+	CPPUNIT_TEST(basic_true_test);
+	CPPUNIT_TEST(basic_false_test);
+
+	 CPPUNIT_TEST_SUITE_END();
+
+public:
+	BasicTest();
+	virtual ~BasicTest();
+	void setUp();
+        void tearDown();
+
+	void basic_true_test(void);
+	void basic_false_test(void);
+};
+
+#endif //SSO_BASICTEST_H
---
+++ tests/src/BasicTest.cpp
@@ -0,0 +1,29 @@
+//
+// Created by plumat on 4/5/16.
+//
+
+#include "BasicTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BasicTest);
+
+BasicTest::BasicTest(){
+}
+
+BasicTest::~BasicTest(){
+}
+
+void BasicTest::setUp(){
+}
+
+void BasicTest::tearDown(){
+}
+
+void BasicTest::basic_true_test(){
+	CPPUNIT_ASSERT(true);
+	CPPUNIT_ASSERT(1==1);
+	CPPUNIT_ASSERT(1+1==2);
+}
+
+void BasicTest::basic_false_test(){
+	CPPUNIT_ASSERT(1==2);
+}
