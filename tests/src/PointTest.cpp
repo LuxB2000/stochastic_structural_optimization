9d6dc9d3aade42491616db02730fec0ed0ad2965
LuxB2000 <j.plumat@gmail.com>
2016-04-16 17:46:59 +1200

    minor modification

--- tests/src/PointTest.cpp
+++ tests/src/PointTest.cpp
@@ -26,19 +26,17 @@ void PointTest::basic_tests() {
     float x=1.0, y=2.0, z=3.5;
 
     // test default constructor
-    Point *a = new Point(x,y,z);
+    Point a = Point(x,y,z);
 
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the x coordinate", x, a->x);
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the y coordinate", y, a->y);
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the z coordinate", z, a->z);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the x coordinate", x, a.x);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the y coordinate", y, a.y);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the z coordinate", z, a.z);
 
     // copy constructor
-    Point *b = new Point(*a);
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the x coordinate", x, b->x);
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the y coordinate", y, b->y);
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the z coordinate", z, b->z);
+    Point b = Point(a);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the x coordinate", x, b.x);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the y coordinate", y, b.y);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Expect the z coordinate", z, b.z);
 
-    delete a;
-    delete b;
 }
 
