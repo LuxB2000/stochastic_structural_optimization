ab3ad5fe1e7f19c80015baa92eca43aba023c46a
LuxB2000 <j.plumat@gmail.com>
2016-04-30 13:55:33 +1200

    minor comments

--- core/headers/Point.h
+++ core/headers/Point.h
@@ -8,6 +8,7 @@
 
 class Point{
 public:
+    // coordinates are given in millimeters in the GLOBAL system.
     Point(float x_coord, float y_coord, float z_coord);
     Point(const Point& p);
     ~Point();
