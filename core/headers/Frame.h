2e911a600069f97d0578c7043d83c7ceec180ae1
LuxB2000 <j.plumat@gmail.com>
2016-06-03 13:54:48 +1200

    test construction

--- core/headers/Frame.h
+++ core/headers/Frame.h
@@ -18,11 +18,44 @@
 #ifndef sso_FRAME_H
 #define sso_FRAME_H
 
+#include "Point.h"
+#include "Types.h"
+
 class Frame{
 public:
 	Frame();
 	~Frame();
 
+};
+
+class TrussFrame : public Frame{
+public:
+	TrussFrame();
+	~TrussFrame();
+};
+
+/*
+ * The structure of the SimpleTrussFrame
+ * Truss 1 and 5 are SimpleTruss (1 param: length)
+ * Truss 2 and 4 are SimpleCornerTruss (3 params: 2 lengths and 1 angle)
+ * Truss 3 is a TopCornerTruss (2 params: 1 length and 1 angle)
+ * Total params: 2*(1+3+2)
+ *    3/\
+ *    o  o
+ * 2 /    \ 4
+ *   |    |
+ *   o    o
+ * 1 |    | 5
+ *   |    |
+ */
+class Simple5TrussFrame : public TrussFrame{
+	Simple5TrussFrame(Point origin, FrameParametersVectorType params );
+	~Simple5TrussFrame();
+private:
+	Simple5TrussFrame();
+	Point* m_origin;
+	FrameParametersVectorType* m_truss_params;
+	static const unsigned int m_number_of_inputs_params = 12;
 };
 
 #endif
--- core/src/Frame.cpp
+++ core/src/Frame.cpp
@@ -6,7 +6,7 @@
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  02/06/16 12:59:40
+ *        Created:  02/05/15 12:59:40
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -17,9 +17,37 @@
 
 #include "Frame.h"
 
+/*
+ * Frame
+ */
 Frame::Frame(){
 }
 
 Frame::~Frame(){
 }
 
+/*
+ * TrussFrame
+ */
+TrussFrame::TrussFrame(){
+}
+
+TrussFrame::~TrussFrame(){
+}
+
+/*
+ * Simple5Truss
+ */
+Simple5TrussFrame::Simple5TrussFrame(){
+	m_origin = new Point(0.0,0.0,0.0);
+	m_truss_params = new FrameParametersVectorType(3,0);
+}
+
+Simple5TrussFrame::Simple5TrussFrame(Point origin, 
+		FrameParametersVectorType params) : Simple5TrussFrame() {
+}
+
+Simple5TrussFrame::~Simple5TrussFrame(){
+	if( m_origin ) delete m_origin;
+	if( m_truss_params ) delete m_truss_params;
+}
