f10d99a4deee74d5395a9fe2ebf4f865001bdea8
LuxB2000 <j.plumat@gmail.com>
2016-06-03 13:49:51 +1200

    Introducting the simplest truss SimpleTruss

---
+++ core/headers/SimpleTruss.h
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  SimpleTruss.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/06/16 13:45:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jerome Plumat (JP)
+ *
+ * =====================================================================================
+ */
+
+#ifndef sso_SIMPLETRUSS_H
+#define sso_SIMPLETRUSS_H
+
+#include "TrussObject.h"
+
+class SimpleTruss : public TrussObject{
+public:
+	SimpleTruss();
+	~SimpleTruss();
+};
+
+#endif
+
---
+++ core/src/SimpleTruss.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  SimpleTruss.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/06/16 13:46:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jerome Plumat (JP)
+ *
+ * =====================================================================================
+ */
+
+#include "SimpleTruss.h"
+
+SimpleTruss::SimpleTruss(){
+}
+
+SimpleTruss::~SimpleTruss(){
+}
+
