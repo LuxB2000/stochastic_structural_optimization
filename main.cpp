7523fbf995cf53bd6192d96b5355188a356cabc3
LuxB2000 <j.plumat@gmail.com>
2016-07-16 16:03:36 +1200

    Introduce a logger
    easylogging++

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-using namespace std;
+
+#include "easylogging++.h"
+
+// Initialize the logger 
+INITIALIZE_EASYLOGGINGPP
+// WARNING: Please note that INITIALIZE_EASYLOGGINGPP should be used once and once-only otherwise you will end up getting compilation errors. This is definiting several extern variables. This means it can be defined only once per application. Best place to put this initialization statement is in file where int main(int, char**) function is defined, right after last include statement.
 
 int main() {
-    cout << "Hello, World!" << endl;
+	std::cout << "Hello, World!" << std::endl;
+
+	LOG(INFO) << "Logging on the info channel";
+
     return 0;
 }
